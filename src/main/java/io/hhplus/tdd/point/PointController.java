b97ee5ad468d70cb24db0097b1c57d6c9e638f34
m5s3 <devmyoungsoo2@gmail.com>
2024-09-27 00:35:33 +0900

    feat(PointController): 내역조회, 포인트 사용 API 구현

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -33,7 +33,7 @@ public class PointController {
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        return List.of();
+        return pointService.showHistories(id);
     }
 
     /**
@@ -55,6 +55,6 @@ public class PointController {
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        return pointService.use(id, amount);
     }
 }
